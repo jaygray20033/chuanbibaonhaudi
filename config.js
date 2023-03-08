4abbce71fcd2d2b26369a717138cf453ad93869e
jaygray20033 <108678381+jaygray20033@users.noreply.github.com>
2023-03-08 22:44:25 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Cậu à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao cậu cứ đi nhầm đường
+    vào tim tớ.`,
+    btnIntro: 'hihi',
+    title: '8/3 vui vẻ.',
+    desc: 'Chúc cậu ngày càng xin đẹp,trẻ trung và luôn giữ nụ cười trên môi(Vì cậu đẹp nhất khi cậu cười :>) CẬU IU TỚ KHUMMM ',
+    btnYes: 'Thích cậu vãi ò <33',
+    btnNo: 'Không đâu,nằm mơ đi :3',
+    question:'Glucozo và Fructozo có cùng công thức phân tử,nhưng glucozo chỉ chọn Brom để phản ứng.Cũng như thế giới cả tỉ người,tớ chỉ iu mình cậu thui <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Tại vì tớ yêu cậu vãi ò <33333333',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Làm ngừi iu tớ nhé!!.',
+    btnAccept: 'Oki,tớ đồng ý! <3',
+    messLink: 'https://www.facebook.com/cuncon.nguyen.359' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
